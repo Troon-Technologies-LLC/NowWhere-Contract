c3df61f88a19073bdba06516bf1b17956e89e022
danishtroon <danish@troontechnologies.com>
2022-06-06 21:03:53 +0500

    add test-cases for nowwhere

--- test/js/assets/constants.js
+++ test/js/assets/constants.js
@@ -1,84 +1,87 @@
 const accountNames = {
-    alice: "Alice",
-    bob: "Bob",
-    charlie: "Charlie",
-    donald: "Donald",
-    emma: "Emma",
-    frank: "Frank",
-    george: "George",
-    harry: "Harry",
-    ian: "Ian",
-    john: "John",
-    keanu: "Keanu",
-    liza: "Liza",
-    micheal: "Micheal",
-    nicolas: "Nicolas",
-    olivia: "Olivia",
-    pamela: "Pamela",
-    quinton: "Quinton",
-    roman: "Roman",
-    sam: "Sam",
-    tom: "Tom",
-    ursula: "Ursula",
-    vin: "Vin",
-    william: "William",
-    xiumin: "Xiumin",
-    yang: "Yang",
-    zoe:"Zoe"
-}
+  alice: "Alice",
+  bob: "Bob",
+  charlie: "Charlie",
+  donald: "Donald",
+  emma: "Emma",
+  frank: "Frank",
+  george: "George",
+  harry: "Harry",
+  ian: "Ian",
+  john: "John",
+  keanu: "Keanu",
+  liza: "Liza",
+  micheal: "Micheal",
+  nicolas: "Nicolas",
+  olivia: "Olivia",
+  pamela: "Pamela",
+  quinton: "Quinton",
+  roman: "Roman",
+  sam: "Sam",
+  tom: "Tom",
+  ursula: "Ursula",
+  vin: "Vin",
+  william: "William",
+  xiumin: "Xiumin",
+  yang: "Yang",
+  zoe: "Zoe",
+};
 
 const contractNames = {
-    fungibleToken: "FungibleToken",
-    flowToken: "FlowToken",
-    nonFungibleToken: "NonFungibleToken",
-    nftContracct: "NFTContract"
-}
+  fungibleToken: "FungibleToken",
+  flowToken: "FlowToken",
+  nonFungibleToken: "NonFungibleToken",
+  nftContract: "NFTContract",
+  nowWhereContract: "NowWhereContract",
+};
 
 const transactions = {
-    mintFT: "mintFT",
-    setupAdminAccount: "setupAdminAccount",
-    addAdminAccount: "addAdminAccount",
-    createBrand: "createBrand",
-    createSchema: "createSchema",
-    createTemplate: "createTemplate",
-    createTemplateStaticData: "createTemplateStaticData",
-    createTemplateNilMutableData: "createTemplateNilMutableData",
-    updateTemplateMutableStaticData: "updateTemplateMutableStaticData"
-}
+  mintFT: "mintFT",
+  setupAdminAccount: "setupAdminAccount",
+  addAdminAccount: "addAdminAccount",
+  createBrand: "createBrand",
+  createSchema: "createSchema",
+  createTemplate: "createTemplate",
+  createTemplateStaticData: "createTemplateStaticData",
+  createTemplateNilMutableData: "createTemplateNilMutableData",
+  updateTemplateMutableStaticData: "updateTemplateMutableStaticData",
+  createDropStaticData: "createDropStaticData",
+  setupAccount: "setupAccount",
+  purchaseDrop: "purchaseDrop",
+  addOwnerVault: "addOwnerVault",
+  purchaseNFTWithFlow: "purchaseNFTWithFlow",
+};
 
 const scripts = {
-    getUserBalance: "getUserBalance",
-    getAllBrands: "getAllBrands",
-    getBrandById: "getBrandById",
-    getBrandCount: "getBrandCount",
-    getBrandName: "getBrandName",
-    getallSchema: "getallSchema",
-    getSchemaById: "getSchemaById",
-    getSchemaCount: "getSchemaCount"
-    
-}
+  getUserBalance: "getUserBalance",
+  getAllBrands: "getAllBrands",
+  getBrandById: "getBrandById",
+  getBrandCount: "getBrandCount",
+  getBrandName: "getBrandName",
+  getallSchema: "getallSchema",
+  getSchemaById: "getSchemaById",
+  getSchemaCount: "getSchemaCount",
+};
 
-const minBalance = 0.0001
-const testingTokenAmount = 100.0
+const minBalance = 0.0001;
+const testingTokenAmount = 100.0;
 
 const flowConfig = {
-    emulatorPort: 8080,
-    gRPCServerPort: 3569,
-    restAPIPort: 8888,
-    basePath:"../../.."
+  emulatorPort: 8080,
+  gRPCServerPort: 3569,
+  restAPIPort: 8888,
+  basePath: "../../..",
+};
 
-}
-
-const timeoutLimit = 100000
+const timeoutLimit = 100000;
 
 export {
-    accountNames,
-    contractNames,
-    transactions,
-    scripts,
-    minBalance,
-    testingTokenAmount,
-    flowConfig,
-    timeoutLimit
-}
-
+  accountNames,
+  contractNames,
+  transactions,
+  scripts,
+  minBalance,
+  testingTokenAmount,
+  flowConfig,
+  timeoutLimit,
+};
--- transactions/createDropStaticData.cdc
+++ transactions/createDropStaticData.cdc
@@ -1,6 +1,6 @@
 import NowWhereContract from "../contracts/NowWhereContract.cdc"
 
-transaction(DropId: UInt64/* , StartDate: UFix64,EndDate: UFix64*/){
+transaction(DropId: UInt64 , StartDate: UFix64, EndDate: UFix64){
     let adminRef: &NowWhereContract.DropAdmin
     prepare(acct: AuthAccount) {
         self.adminRef = acct.borrow<&NowWhereContract.DropAdmin>(from: NowWhereContract.DropAdminStoragePath)
@@ -8,8 +8,6 @@ transaction(DropId: UInt64/* , StartDate: UFix64,EndDate: UFix64*/){
     }
     execute{
         let template : {UInt64:AnyStruct} = {1:"3"}
-        let StartDate : UFix64 = getCurrentBlock().timestamp + 400.0
-        let EndDate : UFix64 = getCurrentBlock().timestamp + 900.0
         self.adminRef.createDrop(dropId: DropId, startDate: StartDate, endDate: EndDate, templates: template)
     }
 }
